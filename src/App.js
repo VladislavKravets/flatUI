dfee603eef69e0681a4b1ea437002433e294f055
vlad.kravets12 <vlad.kravets12@gmail.com>
2023-01-05 15:23:49 +0200

    edited

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
         <>
             <img src={require("./assets/ezgif-2-30aa04409b.gif")} alt=""/>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path={"/palette/:nameId"} element={<Palette/>}/>
-                <Route path="*" element={<Home/>}/>
+                <Route path="/flatUI/#/" element={<Home/>}/>
+                <Route path={"/flatUI/#/palette/:nameId"} element={<Palette/>}/>
+                <Route path="/flatUI/*" element={<Home/>}/>
             </Routes>
         </>
     );
