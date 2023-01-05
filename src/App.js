fe0a101543e612d88a2964f27e847cd7bfb75ef6
vlad.kravets12 <vlad.kravets12@gmail.com>
2023-01-05 14:50:10 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'normalize.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Palette from "./Pages/Palette";
+import Home from "./Pages/Home"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <img src={require("./assets/ezgif-2-30aa04409b.gif")} alt=""/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={ <Home/> }/>
+                    <Route path={"/palette/:nameId"} element={ <Palette/> }/>
+                    <Route path="*" element={ <Home/> }/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
