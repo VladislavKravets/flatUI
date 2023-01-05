0348635f2b4cd0e5a6f6b84691b38d062b3dd1ef
vlad.kravets12 <vlad.kravets12@gmail.com>
2023-01-05 15:06:10 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'normalize.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {HashRouter, Routes, Route} from "react-router-dom";
 import Palette from "./Pages/Palette";
 import Home from "./Pages/Home"
 
@@ -9,13 +9,13 @@ function App() {
     return (
         <>
             <img src={require("./assets/ezgif-2-30aa04409b.gif")} alt=""/>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={ <Home/> }/>
                     <Route path={"/palette/:nameId"} element={ <Palette/> }/>
                     <Route path="*" element={ <Home/> }/>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </>
     );
 }
